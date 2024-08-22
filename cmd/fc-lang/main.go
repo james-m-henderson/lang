a7246b9e88d09437e35c60311a6ecc1869d0c6d2
James <james.henderson@dataductus.com>
2024-08-22 15:17:06 -0400

    Add trace to fc-lang

--- cmd/fc-lang/main.go
+++ cmd/fc-lang/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"os"
+	"runtime/trace"
+	"flag"
 
 	"github.com/freeconf/lang"
 )
 
-const usage = `Usage: %s path-to-socket-file path-to-x-socket-file
+const usage = `Usage: %s [--trace] path-to-socket-file path-to-x-socket-file
 
 path-to-socket-file - name of the domain socket file this executable will create
     that will host the gRPC server defined in fc.proto.
@@ -17,11 +19,31 @@ path-to-x-socket-file - name of the domain socket file that the program calling
 `
 
 func main() {
-	if len(os.Args) < 3 {
+	// Define the --trace flag
+	traceFlag := flag.Bool("trace", false, "Enable tracing and save output to trace.out")
+	flag.Parse()
+
+	// Check if the required arguments are provided
+	if len(flag.Args()) < 2 {
 		log.Fatalf(usage, os.Args[0])
 	}
-	d, err := lang.NewDriver(os.Args[1], os.Args[2])
+
+	// Start tracing if the --trace flag is set
+	if *traceFlag {
+		f, err := os.Create("trace.out")
+		chkerr(err)
+		defer f.Close()
+
+		err = trace.Start(f)
+		chkerr(err)
+		defer trace.Stop()
+	}
+
+	// Initialize the Driver
+	d, err := lang.NewDriver(flag.Args()[0], flag.Args()[1])
 	chkerr(err)
+
+	// Serve the gRPC server
 	chkerr(d.Serve())
 }
 
--- driver.go
+++ driver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"runtime/trace"
 
 	"github.com/freeconf/lang/pb"
 	"github.com/freeconf/yang/fc"
@@ -28,30 +29,38 @@ type DriverStats struct {
 }
 
 func NewDriver(gServerAddr string, xClientAddr string) (*Driver, error) {
+	trace.Log(context.Background(), "Driver", "Initializing new driver")
 	d := &Driver{
 		handles:     newHandlePool(),
 		xclientAddr: xClientAddr,
 	}
+	trace.Log(context.Background(), "Driver", "Checking xClientAddr")
 	// "" only useful for testing
 	if xClientAddr != "" {
+		trace.Log(context.Background(), "Driver", "Creating X Client")
 		if err := d.createXClient(xClientAddr); err != nil {
 			return nil, err
 		}
 	}
+	trace.Log(context.Background(), "Driver", "Creating G Server")
 	if err := d.createGServer(gServerAddr); err != nil {
 		return nil, err
 	}
+	trace.Log(context.Background(), "Driver", "Driver initialization complete")
 	return d, nil
 }
 
 func (d *Driver) createGServer(addr string) error {
+	trace.Log(context.Background(), "Driver", fmt.Sprintf("Creating G Server at %s", addr))
 	var err error
 	if _, ferr := os.Stat(addr); ferr == nil {
 		if err = os.Remove(addr); err != nil {
+			trace.Log(context.Background(), "Driver", fmt.Sprintf("Failed to remove old socket: %s", err))
 			return fmt.Errorf("could not remove old socket %s file. %w", addr, err)
 		}
 	}
 	if d.listener, err = net.Listen("unix", addr); err != nil {
+		trace.Log(context.Background(), "Driver", fmt.Sprintf("Failed to listen on %s: %s", addr, err))
 		return fmt.Errorf("listen error. %s. %w", addr, err)
 	}
 	fc.Debug.Printf("started server on %s", addr)
@@ -63,10 +72,12 @@ func (d *Driver) createGServer(addr string) error {
 	pb.RegisterDeviceServer(d.gserver, &DeviceService{d: d})
 	pb.RegisterProtoServer(d.gserver, &ProtoService{d: d})
 	pb.RegisterFileSystemServer(d.gserver, &FileSystemService{d: d})
+	trace.Log(context.Background(), "Driver", fmt.Sprintf("G Server created and listening at %s", addr))
 	return nil
 }
 
 func (s *Driver) createXClient(addr string) error {
+	trace.Log(context.Background(), "Driver", fmt.Sprintf("Creating X Client for address %s", addr))
 	credentials := insecure.NewCredentials()
 	dialer := func(ctx context.Context, addr string) (net.Conn, error) {
 		var d net.Dialer
@@ -79,23 +90,30 @@ func (s *Driver) createXClient(addr string) error {
 	}
 	channel, err := grpc.Dial(addr, options...)
 	if err != nil {
+		trace.Log(context.Background(), "Driver", fmt.Sprintf("Failed to connect to X Server at %s: %s", addr, err))
 		return fmt.Errorf("failed to start client to x server on %s. %w", addr, err)
 	}
 	fc.Debug.Printf("connected to %s", addr)
 	s.xnodes = pb.NewXNodeClient(channel)
 	s.xfs = pb.NewFileSystemClient(channel)
+	trace.Log(context.Background(), "Driver", fmt.Sprintf("X Client connected to %s", addr))
 	return nil
 }
 
 // Serve is a blocking call that starts the GRPC server
 func (s *Driver) Serve() error {
+	trace.Log(context.Background(), "Driver", "Starting GRPC Server")
 	defer s.listener.Close()
 	if err := s.gserver.Serve(s.listener); err != nil {
+		trace.Log(context.Background(), "Driver", fmt.Sprintf("GRPC Server error: %s", err))
 		return fmt.Errorf("grpc server error. %w", err)
 	}
+	trace.Log(context.Background(), "Driver", "GRPC Server stopped")
 	return nil
 }
 
 func (s *Driver) Stop() {
+	trace.Log(context.Background(), "Driver", "Stopping GRPC Server")
 	s.gserver.Stop()
+	trace.Log(context.Background(), "Driver", "GRPC Server stopped")
 }
